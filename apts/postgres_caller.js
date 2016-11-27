af3eff9bf7632f2eb85561d47f159cb4f1edc699
filipclaesson <filipclaesson@hotmail.com>
2016-11-27 22:59:39 +0100

    adding new database handler, pg-promise

--- apts/postgres_caller.js
+++ apts/postgres_caller.js
@@ -1,22 +1,43 @@
-var pg = require('pg');
+// var pg = require('pg');
 
-function runQuery(query_in, callback) {
-	const conString = 'postgres://postgres:lagge@localhost/booliData'
-	pg.connect(conString, function (err, client, done) {
-		if (err) {
-			return console.error('error fetching client from pool', err)
-		}
-		client.query(query_in, function (err, result) {
-			done()
-			if (err) {
-				return console.error('error happened during query', err)
-			}
-			//console.log(result.rows)
-			callback(result.rows)
-		})
-	})
+// function runQuery(query_in, callback) {
+// 	const conString = 'postgres://postgres:lagge@localhost/booliData'
+// 	pg.connect(conString, function (err, client, done) {
+// 		if (err) {
+// 			return console.error('error fetching client from pool', err)
+// 		}
+// 		client.query(query_in, function (err, result) {
+// 			done()
+// 			if (err) {
+// 				return console.error('error happened during query', err)
+// 			}
+// 			//console.log(result.rows)
+// 			callback(result.rows)
+// 		})
+// 	})
+// }
+// //runQuery("Select 1 as result", function(){console.log("callback")})
+
+var pgp = require('pg-promise')();
+var cn = {
+    host: 'localhost',
+    port: 5432,
+    database: 'booliData',
+    user: 'postgres',
+    password: 'lagge'
+};
+
+function runQuery(query, callback) {
+	var db = pgp(cn);
+	db.any(query, [true])
+    .then(function (data) {
+        callback(data)
+    })
+    .catch(function (error) {
+        // error;
+    });
 }
-//runQuery("Select 1 as result", function(){console.log("callback")})
+console.log(runQuery());
 
 
 
