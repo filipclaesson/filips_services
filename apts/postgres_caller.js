7ee548fe728f8e9adf47b87aa654df5f4f7c797e
filipclaesson <filipclaesson@hotmail.com>
2017-03-30 23:06:19 +0200

    remove commented code in postgres caller

--- apts/postgres_caller.js
+++ apts/postgres_caller.js
@@ -1,22 +1,4 @@
-// var pg = require('pg');
 
-// function runQuery(query_in, callback) {
-// 	const conString = 'postgres://postgres:lagge@localhost/booliData'
-// 	pg.connect(conString, function (err, client, done) {
-// 		if (err) {
-// 			return console.error('error fetching client from pool', err)
-// 		}
-// 		client.query(query_in, function (err, result) {
-// 			done()
-// 			if (err) {
-// 				return console.error('error happened during query', err)
-// 			}
-// 			//console.log(result.rows)
-// 			callback(result.rows)
-// 		})
-// 	})
-// }
-// //runQuery("Select 1 as result", function(){console.log("callback")})
 var pass;
 fs = require('fs')
 //fs.readFile('/home/pi/node_apps/postgres_pass.txt', 'utf8', function (err,data) {
