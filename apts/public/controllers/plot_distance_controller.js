4f8a8a6674eeabcdd5de67af68621f392b96411d
filipclaesson <filipclaesson@hotmail.com>
2017-04-02 02:31:26 +0200

    new faster auery

--- apts/public/controllers/plot_distance_controller.js
+++ apts/public/controllers/plot_distance_controller.js
@@ -84,7 +84,7 @@ function getApartmentsToPlotCircles(){
 
 
 function getApartmentsToPlotRectangle(){
-    query_in = "with base as (  select substring(a.lon::text from 1 for 6) as lon , substring(a.lat::text from 1 for 6) as lat , g.avg_time_to_central::numeric as avg_time_to_central , address , sold_price , sqm  from apartments a  left join geo_data_sl g on (g.lon = round(a.lon::numeric,3)::text and g.lat = round(a.lat::numeric,3)::text)  where object_type = 'Lägenhet' )  select  lon ,lat ,sum(1) as nbr_of_apartments ,min(sqm) as min_sqm ,max(sqm) as max_sqm , round(avg(avg_time_to_central),1) as avg_time , min(address) as address , round(avg(sold_price::numeric/sqm::numeric)/1000)*1000 as avg_price from base group by 1,2 having round(avg(avg_time_to_central),1) > 0"
+    query_in = "select * from visualize_distance"
     
     //query_in = "with base as (  select substring(a.lon::text from 1 for 5) as lon , substring(a.lat::text from 1 for 5) as lat , g.avg_time_to_central::numeric as avg_time_to_central , address , sold_price , sqm  from apartments a  left join geo_data_sl g on (g.lon = round(a.lon::numeric,3)::text and g.lat = round(a.lat::numeric,3)::text)  where object_type = 'Lägenhet' )  select  lon ,lat ,sum(1) as nbr_of_apartments ,min(sqm) as min_sqm ,max(sqm) as max_sqm , round(avg(avg_time_to_central),1) as avg_time , min(address) as address , round(avg(sold_price::numeric/sqm::numeric)/1000)*1000 as avg_price from base group by 1,2 having round(avg(avg_time_to_central),1) > 0"
     
@@ -103,7 +103,7 @@ function getApartmentsToPlotRectangle(){
 
             //  Create Frontend Objects
             for (var i in data){
-                var popupLabel = "count: " + String(data[i]["nbr_of_apartments"]) + ", " + data[i]["min_sqm"] + "≤ sqm ≤" + data[i]["max_sqm"] + ", address: " + data[i]["address"] + ", " + data[i]["avg_price"] + "/kvm";
+                var popupLabel = data[i]["avg_time"] + "min, count: " + String(data[i]["nbr_of_apartments"]) + ", " + data[i]["min_sqm"] + "≤ sqm ≤" + data[i]["max_sqm"] + ", address: " + data[i]["address"] + ", " + data[i]["avg_price"] + "/kvm";
                 var color = getColor(data[i]["avg_time"])
                 var square = createSquare(data[i]["lat"],data[i]["lon"], color, color, 0.5, 20);
                 square.bindPopup(popupLabel);
--- apts/public/source/queries/plot_distance.sql
+++ apts/public/source/queries/plot_distance.sql
@@ -1,3 +1,25 @@
+---------- create visualize_distance table ---------
+create table visualize_distance as (
+select 
+lon_short
+, lat_short
+, sum(1) as nbr_of_apartments
+, min(sqm) as min_sqm
+, max(sqm) as max_sqm
+, round(avg(avg_time_to_central),1) as avg_time
+, min(address) as address
+, round(avg(sold_price/nullif(sqm,0))/1000)*1000 as avg_price
+from view_of_apt_sl group by 1,2
+having round(avg(avg_time_to_central),1) is not null
+order by nbr_of_apartments desc
+)
+
+
+select * from visualize_distance
+
+
+
+
 ------ old db ------
 with base as ( 
 select 
@@ -40,29 +62,21 @@ having round(avg(avg_time_to_central),1) > 0
 with base as ( select substring(a.lon::text from 1 for 6) as lon , substring(a.lat::text from 1 for 6) as lat , g.avg_time_to_central::numeric as avg_time_to_central , address , sold_price , sqm from apartments a left join geo_data_sl g on (g.lon = round(a.lon::numeric,3)::text and g.lat = round(a.lat::numeric,3)::text) ) select lon ,lat , round(avg(avg_time_to_central),1) as avg_time , min(address) as address , round(avg(sold_price::numeric/sqm::numeric)/1000)*1000 as price from base group by 1,2 having round(avg(avg_time_to_central),1) > 0 
 
 -------- square areas, apartments, number of apartmnets per square ------
-with base as ( 
-select substring(a.lon::text from 1 for 6) as lon
-, substring(a.lat::text from 1 for 6) as lat
-, g.avg_time_to_central::numeric as avg_time_to_central
-, address
-, sold_price
-, sqm 
-from apartments a 
-left join geo_data_sl g on (g.lon = round(a.lon::numeric,3)::text and g.lat = round(a.lat::numeric,3)::text) 
-where object_type = 'Lägenhet'
-) 
+
 select 
-lon
-,lat
-,sum(1) as nbr_of_apartments
-,min(sqm) as min_sqm
-,max(sqm) as max_sqm
+lon_short
+, lat_short
+, sum(1) as nbr_of_apartments
+, min(sqm) as min_sqm
+, max(sqm) as max_sqm
 , round(avg(avg_time_to_central),1) as avg_time
 , min(address) as address
-, round(avg(sold_price::numeric/sqm::numeric)/1000)*1000 as avg_price
-from base group by 1,2
-having round(avg(avg_time_to_central),1) > 0
+, round(avg(sold_price/nullif(sqm,0))/1000)*1000 as avg_price
+from view_of_apt_sl group by 1,2
+having round(avg(avg_time_to_central),1) is null
 order by nbr_of_apartments desc
+
+
 
 -- one line 
 with base as (  select substring(a.lon::text from 1 for 6) as lon , substring(a.lat::text from 1 for 6) as lat , g.avg_time_to_central::numeric as avg_time_to_central , address , sold_price , sqm  from apartments a  left join geo_data_sl g on (g.lon = round(a.lon::numeric,3)::text and g.lat = round(a.lat::numeric,3)::text)  where object_type = 'Lägenhet' )  select  lon ,lat ,sum(1) as nbr_of_apartments ,min(sqm) as min_sqm ,max(sqm) as max_sqm , round(avg(avg_time_to_central),1) as avg_time , min(address) as address , round(avg(sold_price::numeric/sqm::numeric)/1000)*1000 as avg_price from base group by 1,2 having round(avg(avg_time_to_central),1) > 0
